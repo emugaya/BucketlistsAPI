cb3b862c8bf9908904a7815249b168fed2734dd0
Ezekiel Mugaya <emugaya@gmail.com>
2017-07-17 20:01:34 +0300

    Updated to apiv1to be able to run tests

--- app/apiv1.py
+++ app/apiv1.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_restplus import Api, Resource, fields
 
 # api = Api(blueprint)
@@ -6,7 +6,8 @@ apiv1_blueprint = Blueprint('apiv1', __name__, url_prefix='/api/v1')
 api = Api(apiv1_blueprint, title='Bucket Application API',version='1.0',
 description="An API to manage bucket lists and their respective items"
 # All API metadatas
-)
+        )
+
 
 #Register namespace for Managing Buckets Lists and there Items
 from .apis.bucketlist import api as ns1
