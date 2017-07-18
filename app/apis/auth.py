f35ba91071c0bc2b3c3844a29875a7ac3ca51720
Ezekiel Mugaya <emugaya@gmail.com>
2017-07-18 20:26:47 +0300

    Created namespace(ns2) auth for handling user registration and login

--- app/apis/auth.py
+++ app/apis/auth.py
@@ -1,35 +1,9 @@
-from flask_restplus import Namespace, Resource, fields
+from flask_restplus import Namespace, Resource, fields, reqparse
+from flask import request, jsonify, make_response
+import json
+from app.models import Bucketlist, Item
+from app import db
+from datetime import datetime, date
 
-api = Namespace('auth/mm', description='Authentication Operations')
-
-auth = api.model('User', {
-    'id': fields.String(required=True, description='The User identifier'),
-    'username': fields.String(required=True,
-                              description="The Application User's name"),
-
-})
-
-CATS = [
-    {'id': 'felix', 'name': 'Felix'},
-]
-
-@api.route('/')
-class CatList(Resource):
-    @api.doc('list_cats')
-    @api.marshal_list_with(auth)
-    def get(self):
-        '''List all cats'''
-        return CATS
-
-@api.route('/<id>')
-@api.param('id', 'The cat identifier')
-@api.response(404, 'Cat not found')
-class Cat(Resource):
-    @api.doc('get_cat')
-    @api.marshal_with(auth)
-    def get(self, id):
-        '''Fetch a cat given its identifier'''
-        for cat in CATS:
-            if cat['id'] == id:
-                return cat
-        api.abort(404)
+api = Namespace('auth',
+                description='Create, Read, Update, Display Buckets and Items')
--- app/apiv1.py
+++ app/apiv1.py
@@ -7,8 +7,10 @@ api = Api(apiv1_blueprint, title='Bucket Application API',version='1.0',
 description="An API to manage bucket lists and their respective items"
 # All API metadatas
         )
-
+#Register namespace for managing User Login and registration
+from .apis.auth import api as ns1
+api.add_namespace(ns1)
 
 #Register namespace for Managing Buckets Lists and there Items
-from .apis.bucketlist import api as ns1
-api.add_namespace(ns1)
+from .apis.bucketlist import api as ns2
+api.add_namespace(ns2)
