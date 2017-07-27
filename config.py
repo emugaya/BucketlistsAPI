aeef176436aa426d6ecd088481a28d2197bae713
Ezekiel Mugaya <emugaya@gmail.com>
2017-07-27 15:00:03 +0300

    Heroku setup

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
 
 class ProductionConfig(Config):
 
-    SQLALCHEMY_DATABASE_URI = "postgres://vohsudxlhkvkvz:24c07d9f20407614a4a6aa463235a34a5ae2f3c9aa452ce38915fca51d446210@ec2-54-75-239-190.eu-west-1.compute.amazonaws.com:5432/d7d7jfgb4midv"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost/bucketlist_db"
 
 
 class DevelopmentConfig(Config):
