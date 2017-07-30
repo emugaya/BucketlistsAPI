89f3707c32df5c557e71a5c2ed3cbe4a932ad66a
Ezekiel Mugaya <emugaya@gmail.com>
2017-07-31 00:37:05 +0300

    Updated file with migrations settings for heroku

--- manage.py
+++ manage.py
@@ -6,9 +6,9 @@ from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
 from app import models
 # from app import models
-# config_name = 'development' #uncomment to implement migrations for heroku app
+config_name = 'development' #uncomment to implement migrations for heroku app
 app = create_app(config_name)
-app = create_app(config_name=os.getenv('APP_SETTINGS'))
+# app = create_app(config_name=os.getenv('APP_SETTINGS'))
 migrate = Migrate(app, db)
 manager = Manager(app)
 
