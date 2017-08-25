3e365a34fee058c2070dc5369cc56df0de80ce84
Ezekiel Mugaya <emugaya@Ezekiels-MacBook-Pro.local>
2017-08-25 09:48:32 +0300

    No chenges made

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ import os
 
 from app import create_app
 
-# config_name = os.getenv('APP_SETTINGS')
-config_name = 'production' #Uncomment before commiting to heroku
+config_name = os.getenv('APP_SETTINGS')
+# config_name = 'production' #Uncomment before commiting to heroku
 
 app = create_app(config_name)
 
